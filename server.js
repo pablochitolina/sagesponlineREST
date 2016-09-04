3a96016056a127414634a4e13455264f41333ce8
Pablo Chitolina <pablochitolina@gmail.com>
2016-09-03 22:34:25 -0300

    as

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(passport.initialize());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "email, _id, Content-Type, Authorization");
+  res.header("Access-Control-Allow-Headers", "email, Content-Type, Authorization");
   next();
 });
 
